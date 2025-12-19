198845cb1062ee8cba1b8b43bd4ada6b89868bf9
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-12-19 12:33:34 -0300

    Basic route structure

--- exercicio01/src/App.tsx
+++ exercicio01/src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./routes/Home"
+import HomeBody from "./routes/Home/Promotion"
+import Promotion from "./routes/Home/HomeBody"
+
 function App() {
 
   return (
-      <h1>Olá mundo</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route index element={<HomeBody />} />
+            <Route path="promotion" element={<Promotion />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
   )
 }
 
---
+++ exercicio01/src/routes/Home/Promotion/index.tsx
@@ -0,0 +1,7 @@
+export default function HomeBody(){
+    return(
+        <main>
+            <h2>Página inicial</h2>
+        </main>
+    );
+}
