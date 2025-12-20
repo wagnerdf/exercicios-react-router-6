b68a3cf7c10096f9ea7d21d71aacb2730560b976
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-12-20 10:40:54 -0300

    Ex1 end

--- exercicio01/src/routes/Home/Promotion/index.tsx
+++ exercicio01/src/routes/Home/Promotion/index.tsx
@@ -1,7 +1,22 @@
-export default function Promotion(){
-    return(
-        <main>
-            <h2>Página inicial</h2>
-        </main>
-    );
+import { Link } from "react-router-dom";
+import Button from "../../../components/Button";
+import Card from "../../../components/Card";
+
+export default function Subscription() {
+  return (
+    <main>
+      <section>
+        <div className="container">
+          <div className="mt30 mb30">
+            <Card title="Página de promoção" />
+          </div>
+          <div className="dflex">
+            <Link to="/sub">
+              <Button text="Quero participar" />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
--- exercicio01/src/routes/Home/Subscription/index.tsx
+++ exercicio01/src/routes/Home/Subscription/index.tsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 import Button from "../../../components/Button";
 import Card from "../../../components/Card";
 
-export default function HomeBody() {
+export default function Promotion() {
   return (
     <main>
       <section>
         <div className="container">
           <div className="mt30 mb30">
-            <Card title="Página inicial" />
+            <Card title="Página de inscrição" />
           </div>
           <div className="dflex">
             <Link to="/promotion">
